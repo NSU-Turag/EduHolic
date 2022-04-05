0f20b435551c1810acf71bc1a8db4871a2273b3a
Turag-nsu <abdullah.turag@northsouth.edu>
2022-04-06 02:14:48 +0600

    pushing

---
+++ myscript.js
@@ -0,0 +1,12 @@
+const ownerButton=document.getElementById('owner-button');
+ownerButton.addEventListener('click',function(){
+    location.href="pages/ownerLogin.html";
+})
+const customerButton=document.getElementById('customer-button');
+customerButton.addEventListener('click',function(){
+    location.href="pages/customerLogin.html";
+})
+const delivaryButton=document.getElementById('delivary-button');
+delivaryButton.addEventListener('click',function(){
+    location.href="pages/delivaryLogin.html";
+})
